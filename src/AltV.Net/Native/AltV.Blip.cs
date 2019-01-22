5614c2ea72bdb4e90202fab29437129543ea3732
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-01-22 20:34:29 +0100

    Add all blip api's

--- src/AltV.Net/Native/AltV.Blip.cs
+++ src/AltV.Net/Native/AltV.Blip.cs
@@ -7,9 +7,29 @@ namespace AltV.Net.Native
     {
         internal static class Blip
         {
+            [DllImport(_dllName, CallingConvention = _callingConvention)]
+            internal static extern bool Blip_IsGlobal(IntPtr blipPointer);
+
+            [DllImport(_dllName, CallingConvention = _callingConvention)]
+            internal static extern bool Blip_IsAttached(IntPtr blipPointer);
+
+            [DllImport(_dllName, CallingConvention = _callingConvention)]
+            internal static extern IntPtr Blip_AttachedTo(IntPtr blipPointer);
+
+            [DllImport(_dllName, CallingConvention = _callingConvention)]
+            internal static extern uint Blip_GetType(IntPtr blipPointer);
+
+            [DllImport(_dllName, CallingConvention = _callingConvention)]
+            internal static extern uint Blip_SetSprite(IntPtr blipPointer, uint sprite);
 
             [DllImport(_dllName, CallingConvention = _callingConvention)]
             internal static extern void Blip_SetColor(IntPtr blipPointer, uint color);
+
+            [DllImport(_dllName, CallingConvention = _callingConvention)]
+            internal static extern void Blip_SetRoute(IntPtr blipPointer, bool state);
+
+            [DllImport(_dllName, CallingConvention = _callingConvention)]
+            internal static extern void Blip_SetRouteColor(IntPtr blipPointer, uint color);
         }
     }
 }
