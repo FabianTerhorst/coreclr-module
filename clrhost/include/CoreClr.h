100cd22a67e750ca3bf8d21118d555d88a9caf43
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-02-11 17:30:28 +0100

    Cleanup header

--- clrhost/include/CoreClr.h
+++ clrhost/include/CoreClr.h
@@ -8,8 +8,10 @@
 //#include <libloaderapi.h>
 #include "dirent_win.h"
 #else
+
 #include <dirent.h>
 #include <dlfcn.h>
+
 #endif
 
 #include <altv-cpp-api/IServer.h>
@@ -17,50 +19,49 @@
 #include <sys/stat.h>
 #include <string.h>
 
-//#include "tail_compare.h"
-
 #ifdef _WIN32
 #define LIST_SEPARATOR ";"
 #else
 #define LIST_SEPARATOR ":"
 #endif
 
-
 #define TAIL_CMP_LT -1
 #define TAIL_CMP_EQ  0
 #define TAIL_CMP_GT +1
 #define TAIL_CMP_KO  9
 
-int tail_cmp ( char *lhs, char *rhs );
+int tail_cmp(char* lhs, char* rhs);
 
-int tail_lt ( char *lhs, char *rhs );
-int tail_eq ( char *lhs, char *rhs );
-int tail_gt ( char *lhs, char *rhs );
+int tail_lt(char* lhs, char* rhs);
 
-class CoreClr
-{
-  public:
-    CoreClr(alt::IServer *server);
+int tail_eq(char* lhs, char* rhs);
+
+int tail_gt(char* lhs, char* rhs);
+
+class CoreClr {
+public:
+    CoreClr(alt::IServer* server);
+
     ~CoreClr();
 
-    bool GetDelegate(alt::IServer *server, void *runtimeHost, unsigned int domainId, const char *moduleName,
-                     const char *classPath, const char *methodName, void **callback);
+    bool GetDelegate(alt::IServer* server, void* runtimeHost, unsigned int domainId, const char* moduleName,
+                     const char* classPath, const char* methodName, void** callback);
 
-    alt::Array<alt::String> getTrustedAssemblies(alt::IServer *server, const char *appPath);
+    alt::Array<alt::String> getTrustedAssemblies(alt::IServer* server, const char* appPath);
 
-    void CreateAppDomain(alt::IServer *server, const char *appPath, const char *libraryPath, void **runtimeHost,
-                         unsigned int *domainId);
+    void CreateAppDomain(alt::IServer* server, const char* appPath, const char* libraryPath, void** runtimeHost,
+                         unsigned int* domainId);
 
-    void Shutdown(alt::IServer *server, void *runtimeHost,
-                           unsigned int domainId);
+    void Shutdown(alt::IServer* server, void* runtimeHost,
+                  unsigned int domainId);
 
     void GetPath(alt::IServer* server, const char* defaultPath);
 
-  private:
+private:
 #ifdef _WIN32
     HMODULE _coreClrLib;
 #else
-    void *_coreClrLib;
+    void* _coreClrLib;
 #endif
     char* runtimeDirectory;
     coreclr_initialize_ptr _initializeCoreCLR;
