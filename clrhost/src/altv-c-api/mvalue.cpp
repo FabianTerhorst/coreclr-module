034a9a5643838826ee60f1b3d9733eeeb9d63b5f
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-02-16 20:52:21 +0100

    ci test 2

--- clrhost/src/altv-c-api/mvalue.cpp
+++ clrhost/src/altv-c-api/mvalue.cpp
@@ -132,6 +132,7 @@ void MValue_CreateDict(alt::MValue* val, const char** keys, uint64_t size, alt::
     mValue = value;*/
     auto map = std::unordered_map<alt::String, alt::MValue>();
     map["bla"] = alt::MValue("123");
+    mValue = alt::MValue(map);
 }
 
 void MValue_CreateFunction(CustomInvoker* val, alt::MValue &mValue) {
