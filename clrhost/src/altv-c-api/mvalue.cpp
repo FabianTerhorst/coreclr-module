0046de74cd37d78216d2435712d7ec145675a73e
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-02-16 19:08:42 +0100

    Improve dictionary

--- clrhost/src/altv-c-api/mvalue.cpp
+++ clrhost/src/altv-c-api/mvalue.cpp
@@ -125,9 +125,9 @@ void MValue_CreateList(alt::MValue val[], uint64_t size, alt::MValueList &valueL
 }
 
 void MValue_CreateDict(alt::MValue* val, const char** keys, uint64_t size, alt::MValue &mValue) {
-    alt::MValue value;
+    alt::MValueDict value;
     for (int i = 0; i < size; i++) {
-        value.Get<alt::MValue::Dict>()[alt::String(keys[i])] = val[i];
+        value[alt::String(keys[i])] = val[i];
     }
     mValue = value;
 }
