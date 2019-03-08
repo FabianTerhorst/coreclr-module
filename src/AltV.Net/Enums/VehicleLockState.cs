9b16fcb05b432e2f2cfe92f35f1de751722c8d4f
pa-rpMoretti <andrew.cade@outlook.com>
2019-03-08 00:29:36 +0000

    Updated VehicleLockState Enum

--- src/AltV.Net/Enums/VehicleLockState.cs
+++ src/AltV.Net/Enums/VehicleLockState.cs
@@ -2,13 +2,13 @@
 {
     public enum VehicleLockState : byte
     {
-        CARLOCK_NONE = 0,
-        CARLOCK_UNLOCKED = 1,
-        CARLOCK_LOCKOUT_FOR_PLAYER_ONLY = 2,
-        CARLOCK_LOCKOUT_PLAYER_ONLY = 3,
-        CARLOCK_LOCKED_PLAYER_INSIDE = 4,
-        CARLOCK_LOCKED_INITIALLY = 5,
-        CARLOCK_FORCE_SHUT_DOORS = 6,
-        CARLOCK_LOCKED_BUT_CAN_BE_DAMAGED = 7
+        None = 0,
+        Unlocked = 1,
+        PlayerOnly = 2,
+        LockoutPlayerOnly = 3,
+        LockPlayerInside = 4,
+        InitiallyLocked = 5,
+        ForceDoorsShut = 6,
+        LockedCanBeDamaged = 7
     }
 }
