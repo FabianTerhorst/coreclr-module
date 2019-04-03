6f47d101f76dae35a877d59f63c285590ea4bd93
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-04-03 17:21:48 +0200

    Fix rotation code style

--- api/AltV.Net/Data/Rotation.cs
+++ api/AltV.Net/Data/Rotation.cs
@@ -8,34 +8,34 @@ namespace AltV.Net.Data
     {
         public static Rotation Zero = new Rotation
         {
-            roll = 0,
-            pitch = 0,
-            yaw = 0
+            Roll = 0,
+            Pitch = 0,
+            Yaw = 0
         };
 
-        public float roll;
-        public float pitch;
-        public float yaw;
+        public float Roll;
+        public float Pitch;
+        public float Yaw;
 
         public Rotation(float roll, float pitch, float yaw)
         {
-            this.roll = roll;
-            this.pitch = pitch;
-            this.yaw = yaw;
+            Roll = roll;
+            Pitch = pitch;
+            Yaw = yaw;
         }
 
         public override string ToString()
         {
-            return $"Rotation(roll: {roll}, pitch: {pitch}, yaw: {yaw})";
+            return $"Rotation(roll: {Roll}, pitch: {Pitch}, yaw: {Yaw})";
         }
 
         public static implicit operator Vector3(Rotation rotation)
         {
             return new Vector3
             {
-                X = rotation.roll,
-                Y = rotation.pitch,
-                Z = rotation.yaw
+                X = rotation.Roll,
+                Y = rotation.Pitch,
+                Z = rotation.Yaw
             };
         }
 
@@ -43,9 +43,9 @@ namespace AltV.Net.Data
         {
             return new Rotation
             {
-                roll = vector3.X,
-                pitch = vector3.Y,
-                yaw = vector3.Z
+                Roll = vector3.X,
+                Pitch = vector3.Y,
+                Yaw = vector3.Z
             };
         }
     }
