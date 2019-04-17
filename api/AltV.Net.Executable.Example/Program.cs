31fbc6293f24c4bb2427983d3f908f46f4e3660a
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-04-17 17:10:55 +0200

    Cleanup executable example

--- api/AltV.Net.Executable.Example/Program.cs
+++ api/AltV.Net.Executable.Example/Program.cs
@@ -1,13 +1,7 @@
-﻿using System;
-
-namespace AltV.Net.Executable.Example
+﻿namespace AltV.Net.Executable.Example
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            new MyResource().Start(args);
-        }
+        static void Main(string[] args) => new MyResource().Start(args);
     }
 }
