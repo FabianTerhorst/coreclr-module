38a67ee393c88eb4b5b4ecd70acae827cc40cbdf
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-04-17 17:20:39 +0200

    Cleanup executable example

--- api/AltV.Net.Executable.Example/MyResource.cs
+++ api/AltV.Net.Executable.Example/MyResource.cs
@@ -1,18 +1,20 @@
+using System;
+
 namespace AltV.Net.Executable.Example
 {
-    class MyResource : Resource
+    internal class MyResource : Resource
     {
         public override void OnStart()
         {
-            Alt.OnServerEvent += (name, args) =>
-            {
-                Alt.Log(name + " " + args.Length);
-            };
+            Alt.OnServerEvent += (name, args) => { Alt.Log(name + " " + args.Length); };
             Alt.Emit("test");
+
+            Console.WriteLine("Started");
         }
 
         public override void OnStop()
         {
+            Console.WriteLine("Stopped");
         }
     }
 }
