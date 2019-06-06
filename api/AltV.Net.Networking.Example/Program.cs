242df1b564f4b5bd4bbbea29d91c42fb89f62479
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-06-06 15:21:55 +0200

    Implement all server events

--- api/AltV.Net.Networking.Example/Program.cs
+++ api/AltV.Net.Networking.Example/Program.cs
@@ -15,8 +15,11 @@ namespace AltV.Net.Networking.Example
                 new ClientTokenProvider(), new NetworkingClientFactory(),
                 new NonePlayerAuthenticationProviderFactory(), new ClientEntityStreamingHandlerFactory()));
             var data = new Dictionary<string, object>();
+            var data2 = new Dictionary<string, object>();
+            data["bla"] = "123";
+            data["bla2"] = 1235;
             AltNetworking.CreateEntity(new Position {X = 0, Y = 0, Z = 0}, 1, 50, data);
-            AltNetworking.CreateEntity(new Position {X = 1, Y = 1, Z = 1}, 1, 50, data);
+            AltNetworking.CreateEntity(new Position {X = 1, Y = 1, Z = 1}, 1, 50, data2);
             AltNetworking.OnEntityStreamIn = (entity, client) => { Console.WriteLine("streamed in " + entity.Id + " in client " + client.Token); };
             AltNetworking.OnEntityStreamOut = (entity, client) => { Console.WriteLine("streamed out " + entity.Id + " in client " + client.Token); };
             Console.ReadKey();
--- api/AltV.Net.NetworkingEntity/Client/websocket.mjs
+++ api/AltV.Net.NetworkingEntity/Client/websocket.mjs
@@ -31,6 +31,64 @@ class WebSocket {
                 if (obj.send != null) {
                     const entities = obj.send.entities;
                     this.entityRepository.setEntities(entities);
+                } else if (obj.dataChange != null) {
+                    const dataChange = obj.dataChange;
+                    if (this.entityRepository.entities.has(dataChange.id)) {
+                        const entity = this.entityRepository.entities.get(dataChange.id);
+                        if (!entity.data) {
+                            entity.data = {};
+                        }
+                        entity.data[dataChange.key] = dataChange.value;
+                        console.log("data changed", entity);
+                    }
+                } else if (obj.positionChange) {
+                    const positionChange = obj.positionChange;
+                    if (this.entityRepository.entities.has(positionChange.id)) {
+                        const entity = this.entityRepository.entities.get(positionChange.id);
+                        entity.position = positionChange.position;
+                        this.entityRepository.updateWorker();
+                        console.log("position changed", entity);
+                    }
+                } else if (obj.rangeChange) {
+                    const rangeChange = obj.rangeChange;
+                    if (this.entityRepository.entities.has(rangeChange.id)) {
+                        const entity = this.entityRepository.entities.get(rangeChange.id);
+                        entity.range = rangeChange.range;
+                        this.entityRepository.updateWorker();
+                        console.log("range changed", entity);
+                    }
+                } else if (obj.delete) {
+                    const deleteEvent = obj.delete;
+                    if (this.entityRepository.entities.delete(deleteEvent.id)) {
+                        this.entityRepository.updateWorker();
+                        console.log("entity deleted", deleteEvent.id);
+                    }
+                } else if (obj.create) {
+                    const createEvent = obj.create;
+                    if (createEvent.entity) {
+                        this.entityRepository.entities.set(createEvent.entity.id, createEvent.entity);
+                        this.entityRepository.updateWorker();
+                    }
+                } else if (obj.multipleDataChange) {
+                    const multipleDataChange = obj.multipleDataChange;
+                    if (this.entityRepository.entities.has(multipleDataChange.id)) {
+                        const entity = this.entityRepository.entities.get(multipleDataChange.id);
+                        if (!entity.data) {
+                            entity.data = {};
+                        }
+                        for (const [newKey, newValue] of multipleDataChange.data) {
+                            entity.data[newKey] = newValue;
+                        }
+                        console.log("multiple data change", entity);
+                    }
+                } else if (obj.dimensionChange) {
+                    const dimensionChange = obj.dimensionChange;
+                    if (this.entityRepository.entities.has(dimensionChange.id)) {
+                        const entity = this.entityRepository.entities.get(dimensionChange.id);
+                        entity.dimension = dimensionChange.dimension;
+                        this.entityRepository.updateWorker();
+                        console.log("dimension changed", entity);
+                    }
                 }
             });
         };
