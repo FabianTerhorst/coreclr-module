8c8b79cb0c540fb685163a6358718dfd9769932d
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-06-11 18:25:10 +0200

    Update versions

--- runtime/linux-build.sh
+++ runtime/linux-build.sh
@@ -1,3 +1,3 @@
-cmake -B"cmake-build-linux"
+cmake -B"cmake-build-linux" -DCMAKE_CXX_FLAGS="-fsanitize=address  -fsanitize=leak -g" -DCMAKE_C_FLAGS="-fsanitize=address  -fsanitize=leak -g" -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address  -fsanitize=leak" -DCMAKE_MODULE_LINKER_FLAGS="-fsanitize=address  -fsanitize=leak"
 cd cmake-build-linux
 make
