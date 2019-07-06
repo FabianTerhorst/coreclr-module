8a77b49bb16ac2e2d1f4120c856612c499104323
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-07-06 16:28:50 +0200

    Disable disconnect reason

--- runtime/src/CSharpResource.cpp
+++ runtime/src/CSharpResource.cpp
@@ -298,7 +298,7 @@ bool CSharpResource::OnEvent(const alt::CEvent* ev) {
         case alt::CEvent::Type::PLAYER_DISCONNECT: {
             auto disconnectEvent = reinterpret_cast<const alt::CPlayerDisconnectEvent*>(ev);
             auto disconnectPlayer = disconnectEvent->GetTarget();
-            auto reason = disconnectEvent->GetReason();
+            /*auto reason = disconnectEvent->GetReason();
             if (reason != nullptr && reason.GetSize() > 0) {
                 auto reasonCStr = reason.CStr();
                 if (reasonCStr != nullptr) {
@@ -307,11 +307,11 @@ bool CSharpResource::OnEvent(const alt::CEvent* ev) {
                 } else {
                     OnPlayerDisconnectDelegate(disconnectPlayer,
                                                "");
-                }
-            } else {
+                }*/
+            //} else {
                 OnPlayerDisconnectDelegate(disconnectPlayer,
                                            "");
-            }
+            //}
             break;
         }
         case alt::CEvent::Type::REMOVE_ENTITY_EVENT: {
