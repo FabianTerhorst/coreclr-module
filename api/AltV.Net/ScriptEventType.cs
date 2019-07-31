50972b1726232fbeec5e13e7599034fc8e18ecfe
Fabian Terhorst <fabian.terhorst@gmail.com>
2019-08-01 01:22:48 +0200

    Add all attribute events

--- api/AltV.Net/ScriptEventType.cs
+++ api/AltV.Net/ScriptEventType.cs
@@ -2,8 +2,23 @@ namespace AltV.Net
 {
     public enum ScriptEventType
     {
+        Checkpoint,
         PlayerConnect,
+        PlayerDamage,
+        PlayerDead,
+        PlayerDisconnect,
+        PlayerRemove,
+        VehicleRemove,
+        PlayerChangeVehicleSeat,
+        PlayerEnterVehicle,
+        PlayerLeaveVehicle,
+        PlayerEvent,
+        PlayerCustomEvent,
         ServerEvent,
-        VehicleRemove
+        ServerCustomEvent,
+        ConsoleCommand,
+        MetaDataChange,
+        SyncedMetaDataChange,
+        ColShape
     }
 }
