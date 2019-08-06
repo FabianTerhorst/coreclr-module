4a92a6334b33463bbf0fc202666d4a7e50ba4c20
Moretti <moretti@ls-v.com>
2019-08-06 17:40:17 +0100

    Added WeatherType

---
+++ api/AltV.Net/Enums/WeatherType.cs
@@ -0,0 +1,21 @@
+﻿namespace AltV.Net.Enums
+{
+    public enum WeatherType
+    {
+        ExtraSunny,
+        Clear,
+        Clouds,
+        Smog,
+        Foggy,
+        Overcast,
+        Rain,
+        Thunder,
+        Clearing,
+        Neutral,
+        Snow,
+        Blizzard,
+        Snowlight,
+        Xmas,
+        Halloween
+    }
+}
