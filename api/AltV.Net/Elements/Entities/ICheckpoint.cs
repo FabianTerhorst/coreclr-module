093763ecd51ed9142e310f5ca7d14d427495971c
Paul James Schell <45160099+pauljschell@users.noreply.github.com>
2019-10-08 17:05:12 -0400

    Add comments (#81) (#86)
    Add documentation to ICheckpoint interface

--- api/AltV.Net/Elements/Entities/ICheckpoint.cs
+++ api/AltV.Net/Elements/Entities/ICheckpoint.cs
@@ -4,14 +4,53 @@ namespace AltV.Net.Elements.Entities
 {
     public interface ICheckpoint : IWorldObject
     {
+        /// <summary>
+        /// Gets whether the checkpoint is global
+        /// </summary>
         bool IsGlobal { get; }
+        
+        /// <summary>
+        /// Returns the checkpoint type
+        /// </summary>
         byte CheckpointType { get; }
+        
+        /// <summary>
+        /// Gets the height
+        /// </summary>
         float Height { get; }
+        
+        /// <summary>
+        /// Gets the radius
+        /// </summary>
         float Radius { get; }
+        
+        /// <summary>
+        /// Obtains the color in Rgba format
+        /// </summary>
         Rgba Color { get; }
+        
+        /// <summary>
+        /// Returns the player for whom the checkpoint is visible
+        /// </summary>
         IPlayer Target { get; }
+        
+        /// <summary>
+        /// Returns if the player is inside the checkpoint
+        /// </summary>
+        /// <param name="player">The player</param>
+        /// <exception cref="EntityRemovedException">This entity was removed</exception>
         bool IsPlayerIn(IPlayer player);
+        
+        /// <summary>
+        /// Returns if the vehicle is inside the checkpoint
+        /// </summary>
+        /// <param name="vehicle">The vehicle</param>
+        /// <exception cref="EntityRemovedException">This entity was removed</exception>
         bool IsVehicleIn(IVehicle vehicle);
+        
+        /// <summary>
+        /// Removes the checkpoint
+        /// </summary>
         void Remove();
     }
 }
