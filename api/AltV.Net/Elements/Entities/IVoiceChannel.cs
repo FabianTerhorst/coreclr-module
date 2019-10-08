1b456294e050556569c003873b2ea8b636d2cf71
Paul James Schell <45160099+pauljschell@users.noreply.github.com>
2019-10-08 07:19:49 -0400

    Add comments (#82) (#87)
    Add documentation to IVoiceChannel interface

--- api/AltV.Net/Elements/Entities/IVoiceChannel.cs
+++ api/AltV.Net/Elements/Entities/IVoiceChannel.cs
@@ -1,15 +1,62 @@
 namespace AltV.Net.Elements.Entities
 {
     public interface IVoiceChannel : IBaseObject
-    {
+    {        
+        /// <summary>
+        /// Adds the player to the voice channel
+        /// </summary>
+        /// <param name="player">The player</param>
+        /// <exception cref="EntityRemovedException">This entity was removed</exception>
         void AddPlayer(IPlayer player);
+        
+        /// <summary>
+        /// Removes the player from the voice channel
+        /// </summary>
+        /// <param name="player">The player</param>
+        /// <exception cref="EntityRemovedException">This entity was removed</exception>
         void RemovePlayer(IPlayer player);
+        
+        /// <summary>
+        /// Mutes the player in the voice channel
+        /// </summary>
+        /// <param name="player">The player</param>
+        /// <exception cref="EntityRemovedException">This entity was removed</exception>
         void MutePlayer(IPlayer player);
+        
+        /// <summary>
+        /// Unmutes the player in the voice channel
+        /// </summary>
+        /// <param name="player">The player</param>
+        /// <exception cref="EntityRemovedException">This entity was removed</exception>
         void UnmutePlayer(IPlayer player);
+        
+        /// <summary>
+        /// Returns if the player is connected to the voice channel
+        /// </summary>
+        /// <param name="player">The player</param>
+        /// <exception cref="EntityRemovedException">This entity was removed</exception>
         bool IsPlayerConnected(IPlayer player);
+        
+        /// <summary>
+        /// Returns if the player is muted in the voice channel
+        /// </summary>
+        /// <param name="player">The player</param>
+        /// <exception cref="EntityRemovedException">This entity was removed</exception>
         bool IsPlayerMuted(IPlayer player);
+        
+        /// <summary>
+        /// Returns if the voice channel is spatial
+        /// </summary>
         bool IsSpatial { get; }
+        
+        /// <summary>
+        /// Returns the maximum distance
+        /// </summary>
         float MaxDistance { get; }
+        
+        /// <summary>
+        /// Removes the voice channel
+        /// </summary>
         void Remove();
     }
 }
