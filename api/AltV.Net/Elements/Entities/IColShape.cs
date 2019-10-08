fee579291c5b3d2393baafbf038c88fc9152ca05
Taupaulogy <pauljschell@gmail.com>
2019-10-07 21:56:23 -0400

    Add comments (#80)
    Add documentation to IColShape interface

--- api/AltV.Net/Elements/Entities/IColShape.cs
+++ api/AltV.Net/Elements/Entities/IColShape.cs
@@ -2,8 +2,21 @@ namespace AltV.Net.Elements.Entities
 {
     public interface IColShape : IWorldObject
     {
+        /// <summary>
+        /// Obtains the enumerated value of collision shape of the entity
+        /// </summary>
         ColShapeType ColShapeType { get; }
+		
+        /// <summary>
+        /// Checks whether the entity exists
+        /// </summary>
+        /// <param name="entity">The entity</param>
+        /// <exception cref="EntityRemovedException">This entity was removed</exception>
         bool IsEntityIn(IEntity entity);
+		
+        /// <summary>
+        /// Removes the collision shape
+        /// </summary>
         void Remove();
     }
 }
