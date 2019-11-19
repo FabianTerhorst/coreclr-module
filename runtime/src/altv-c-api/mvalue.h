14fd9bbafd100abca8d4a80f4e79cba0db2c2a07
fabian <fabian.terhorst@gmail.com>
2019-11-19 19:49:19 +0100

    Update mvalue.h

--- runtime/src/altv-c-api/mvalue.h
+++ runtime/src/altv-c-api/mvalue.h
@@ -20,18 +20,6 @@ extern "C"
 EXPORT CustomInvoker* Invoker_Create(CSharpResourceImpl* resource, MValueFunctionCallback val);
 EXPORT void Invoker_Destroy(CSharpResourceImpl* resource, CustomInvoker* val);
 
-/*EXPORT bool MValue_GetBool(alt::MValueBool &mValue);
-EXPORT int64_t MValue_GetInt(alt::MValueInt &mValue);
-EXPORT uint64_t MValue_GetUInt(alt::MValueUInt &mValue);
-EXPORT double MValue_GetDouble(alt::MValueDouble &mValue);
-EXPORT void MValue_GetString(alt::MValueString &mValue, const char*&value, uint64_t &size);
-EXPORT void MValue_GetList(alt::MValueList &mValue, alt::Array<alt::MValue> &value);
-EXPORT void
-MValue_GetDict(alt::MValueDict &mValue, alt::Array<alt::String> &keys, alt::Array<alt::MValueConst> &values);
-EXPORT void* MValue_GetEntity(alt::MValueBaseObject &mValue, alt::IBaseObject::Type &type);
-EXPORT void MValue_CallFunction(alt::MValueFunction &mValue, alt::MValue val[], int32_t size, alt::MValue &result);
-EXPORT void MValue_Dispose(alt::MValue* mValue);*/
-
 EXPORT bool MValueConst_GetBool(alt::MValueConst* mValueConst);
 EXPORT int64_t MValueConst_GetInt(alt::MValueConst* mValueConst);
 EXPORT uint64_t MValueConst_GetUInt(alt::MValueConst* mValueConst);
