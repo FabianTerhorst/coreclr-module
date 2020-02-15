c2b309b270f6b9aa00346f749663dbd00cef353f
fabian <fabian.terhorst@gmail.com>
2020-02-15 03:57:52 +0100

    Add Grid3 andLimitedGrid3 with better scaling add

---
+++ api/AltV.Net.EntitySync.Tests/LimitedGrid3Tests.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using AltV.Net.EntitySync.SpatialPartitions;
+using NUnit.Framework;
+
+namespace AltV.Net.EntitySync.Tests
+{
+    public class LimitedGrid3Tests
+    {
+        private LimitedGrid3 grid2;
+
+        [SetUp]
+        public void Setup()
+        {
+            AltEntitySync.Init(1, 500,
+                repository => new MockNetworkLayer(repository),
+                () => new LimitedGrid(50_000, 50_000, 100, 10_000, 10_000, 3),
+                new IdProvider());
+            grid2 = new LimitedGrid3(50_000, 50_000, 100, 10_000, 10_000, 3);
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            var position = GetRandomVector3();
+            var entity = new Entity(1, position, 0, 1);
+            var entity2 = new Entity(1, new Vector3(position.X + 1, position.Y, position.Z), 0, 1);
+            var entity3 = new Entity(1, new Vector3(position.X, position.Y + 1, position.Z), 0, 1);
+            var entity4 = new Entity(1, new Vector3(position.X, position.Y + 1, position.Z + 1), 0, 2);
+            grid2.Add(entity);
+            grid2.Add(entity2);
+            grid2.Add(entity3);
+            grid2.Add(entity4);
+            using (var enumerator = grid2.Find(position, 0).GetEnumerator())
+            {
+                var currSet = new HashSet<IEntity>();
+                while (enumerator.MoveNext())
+                {
+                    currSet.Add(enumerator.Current);
+                }
+                Assert.True(currSet.Contains(entity));
+                Assert.True(currSet.Contains(entity2));
+                Assert.True(currSet.Contains(entity3));
+                Assert.False(currSet.Contains(entity4));
+            }
+        }
+
+        private static Vector3 GetRandomVector3()
+        {
+            return new Vector3((float) GetRandomNumber(0, 49_899), (float) GetRandomNumber(0, 49_899),
+                (float) GetRandomNumber(0, 49_899));
+        }
+
+        private static double GetRandomNumber(double minimum, double maximum)
+        {
+            var random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
---
+++ api/AltV.Net.EntitySync/SpatialPartitions/Grid3.cs
@@ -0,0 +1,437 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace AltV.Net.EntitySync.SpatialPartitions
+{
+    public class Grid3 : SpatialPartition
+    {
+        private static readonly float Tolerance = 0.013F; //0.01318359375F;
+
+        // x-index, y-index, col shapes
+        protected readonly List<IEntity>[][] entityAreas;
+
+        protected readonly int maxX;
+
+        protected readonly int maxY;
+
+        protected readonly int areaSize;
+
+        protected readonly int xOffset;
+
+        protected readonly int yOffset;
+
+        protected readonly int maxXAreaIndex;
+
+        protected readonly int maxYAreaIndex;
+
+        /// <summary>
+        /// The constructor of the grid spatial partition algorithm
+        /// </summary>
+        /// <param name="maxX">The max x value</param>
+        /// <param name="maxY">The max y value</param>
+        /// <param name="areaSize">The Size of a grid area</param>
+        /// <param name="xOffset"></param>
+        /// <param name="yOffset"></param>
+        public Grid3(int maxX, int maxY, int areaSize, int xOffset, int yOffset)
+        {
+            this.maxX = maxX + xOffset;
+            this.maxY = maxY + yOffset;
+            this.areaSize = areaSize;
+            this.xOffset = xOffset;
+            this.yOffset = yOffset;
+
+            maxXAreaIndex = this.maxX / areaSize;
+            maxYAreaIndex = this.maxY / areaSize;
+
+            entityAreas = new List<IEntity>[maxXAreaIndex][];
+
+            for (var i = 0; i < maxXAreaIndex; i++)
+            {
+                entityAreas[i] = new List<IEntity>[maxYAreaIndex];
+                for (var j = 0; j < maxYAreaIndex; j++)
+                {
+                    entityAreas[i][j] = new List<IEntity>();
+                }
+            }
+        }
+
+        public override void Add(IEntity entity)
+        {
+            var entityPositionX = entity.Position.X + xOffset;
+            var entityPositionY = entity.Position.Y + yOffset;
+            var range = entity.Range;
+
+            var squareMaxX = entityPositionX + range;
+            var squareMaxY = entityPositionY + range;
+            var squareMinX = entityPositionX - range;
+            var squareMinY = entityPositionY - range;
+
+            // we actually have a circle but we use this as a square for performance reasons
+            // we now find all areas that are inside this square
+            // We first use starting y index to start filling
+            var startingYIndex = (int) Math.Floor(squareMinY / areaSize);
+            // We now define starting x index to start filling
+            var startingXIndex = (int) Math.Floor(squareMinX / areaSize);
+            // Also define stopping indexes
+            var stoppingYIndex =
+                (int) Math.Ceiling(squareMaxY / areaSize);
+            var stoppingXIndex =
+                (int) Math.Ceiling(squareMaxX / areaSize);
+
+            if (range == 0 || startingYIndex < 0 || startingXIndex < 0 ||
+                stoppingYIndex >= maxYAreaIndex ||
+                stoppingXIndex >= maxXAreaIndex) return;
+
+            // Now fill all areas from min {x, y} to max {x, y}
+            for (var j = startingXIndex; j <= stoppingXIndex; j++)
+            {
+                var xArr = entityAreas[j];
+                for (var i = startingYIndex; i <= stoppingYIndex; i++)
+                {
+                    xArr[i].Add(entity);
+                }
+            }
+        }
+
+        public override void Remove(IEntity entity)
+        {
+            var entityPositionX = entity.Position.X + xOffset;
+            var entityPositionY = entity.Position.Y + yOffset;
+            var range = entity.Range;
+            var id = entity.Id;
+            var type = entity.Type;
+
+            var squareMaxX = entityPositionX + range;
+            var squareMaxY = entityPositionY + range;
+            var squareMinX = entityPositionX - range;
+            var squareMinY = entityPositionY - range;
+
+            // we actually have a circle but we use this as a square for performance reasons
+            // we now find all areas that are inside this square
+            // We first use starting y index to start filling
+            var startingYIndex = (int) Math.Floor(squareMinY / areaSize);
+            // We now define starting x index to start filling
+            var startingXIndex = (int) Math.Floor(squareMinX / areaSize);
+            // Also define stopping indexes
+            var stoppingYIndex =
+                (int) Math.Ceiling(squareMaxY / areaSize);
+            var stoppingXIndex =
+                (int) Math.Ceiling(squareMaxX / areaSize);
+
+            if (range == 0 || startingYIndex < 0 || startingXIndex < 0 ||
+                stoppingYIndex > maxYAreaIndex ||
+                stoppingXIndex > maxXAreaIndex) return;
+
+            // Now remove entity from all areas from min {x, y} to max {x, y}
+            for (var j = startingXIndex; j <= stoppingXIndex; j++)
+            {
+                var xArr = entityAreas[j];
+                for (var i = startingYIndex; i <= stoppingYIndex; i++)
+                {
+                    var arr = xArr[i];
+                    var length = arr.Count;
+                    int k;
+                    var found = false;
+                    for (k = 0; k < length; k++)
+                    {
+                        var currEntity = arr[k];
+                        if (currEntity.Id != id || currEntity.Type != type) continue;
+                        found = true;
+                        break;
+                    }
+
+                    if (!found) continue;
+
+                    xArr[i].RemoveAt(k);
+                }
+            }
+        }
+
+        public override void UpdateEntityPosition(IEntity entity, in Vector3 newPosition)
+        {
+            var oldEntityPositionX = entity.Position.X + xOffset;
+            var oldEntityPositionY = entity.Position.Y + yOffset;
+            var newEntityPositionX = newPosition.X + xOffset;
+            var newEntityPositionY = newPosition.Y + yOffset;
+            var range = entity.Range;
+            var id = entity.Id;
+            var type = entity.Type;
+
+            var oldSquareMaxX = oldEntityPositionX + range;
+            var oldSquareMaxY = oldEntityPositionY + range;
+            var oldSquareMinX = oldEntityPositionX - range;
+            var oldSquareMinY = oldEntityPositionY - range;
+
+            var newSquareMaxX = newEntityPositionX + range;
+            var newSquareMaxY = newEntityPositionY + range;
+            var newSquareMinX = newEntityPositionX - range;
+            var newSquareMinY = newEntityPositionY - range;
+
+            if (range == 0 || oldSquareMinX < 0 || oldSquareMinY < 0 ||
+                oldSquareMaxX > maxX ||
+                oldSquareMaxY > maxY || newSquareMinX < 0 || newSquareMinY < 0 ||
+                newSquareMaxX > maxX ||
+                newSquareMaxY > maxY) return;
+
+            // we actually have a circle but we use this as a square for performance reasons
+            // we now find all areas that are inside this square
+            // We first use starting y index to start filling
+            var oldStartingYIndex = (int) Math.Floor(oldSquareMinY / areaSize);
+            // We now define starting x index to start filling
+            var oldStartingXIndex = (int) Math.Floor(oldSquareMinX / areaSize);
+            // Also define stopping indexes
+            var oldStoppingYIndex =
+                (int) Math.Ceiling(oldSquareMaxY / areaSize);
+            var oldStoppingXIndex =
+                (int) Math.Ceiling(oldSquareMaxX / areaSize);
+
+            // we actually have a circle but we use this as a square for performance reasons
+            // we now find all areas that are inside this square
+            // We first use starting y index to start filling
+            var newStartingYIndex = (int) Math.Floor(newSquareMinY / areaSize);
+            // We now define starting x index to start filling
+            var newStartingXIndex = (int) Math.Floor(newSquareMinX / areaSize);
+            // Also define stopping indexes
+            var newStoppingYIndex =
+                (int) Math.Ceiling(newSquareMaxY / areaSize);
+            var newStoppingXIndex =
+                (int) Math.Ceiling(newSquareMaxX / areaSize);
+
+            //TODO: do later checking for overlaps between the grid areas in the two dimensional array
+            //  --    --    --    --   
+            // |xy|  |xy|  |yy|  |  |    
+            // |yx|  |yx|  |yy|  |  |
+            //  --    --    --    --  
+            //
+            //  --    --    --    --   
+            // |xy|  |xy|  |yy|  |  |    
+            // |yx|  |yx|  |yy|  |  |
+            //  --    --    --    --  
+            //
+            //  --    --    --    --   
+            // |yy|  |yy|  |yy|  |  |    
+            // |yy|  |yy|  |yy|  |  |
+            //  --    --    --    --  
+            //
+            //  --    --    --    --   
+            // |  |  |  |  |  |  |  |    
+            // |  |  |  |  |  |  |  |
+            //  --    --    --    --  
+            // Now we have to check if some of the (oldStartingYIndex, oldStoppingYIndex) areas are inside (newStartingYIndex, newStoppingYIndex)
+            // Now we have to check if some of the (oldStartingXIndex, oldStoppingXIndex) areas are inside (newStartingXIndex, newStoppingXIndex)
+
+
+            for (var j = oldStartingXIndex; j <= oldStoppingXIndex; j++)
+            {
+                var xArr = entityAreas[j];
+                for (var i = oldStartingYIndex; i <= oldStoppingYIndex; i++)
+                {
+                    //TODO: Now we check if (i,j) is inside the new position range, so we don't have to delete it
+                    var arr = xArr[i];
+                    var length = arr.Count;
+                    int k;
+                    var found = false;
+                    for (k = 0; k < length; k++)
+                    {
+                        var currEntity = arr[k];
+                        if (currEntity.Id != id || currEntity.Type != type) continue;
+                        found = true;
+                        break;
+                    }
+
+                    if (!found) continue;
+                    xArr[i].RemoveAt(k);
+                }
+            }
+
+            for (var j = newStartingXIndex; j <= newStoppingXIndex; j++)
+            {
+                var xArr = entityAreas[j];
+                for (var i = newStartingYIndex; i <= newStoppingYIndex; i++)
+                {
+                    xArr[i].Add(entity);
+                }
+            }
+        }
+
+        public override void UpdateEntityRange(IEntity entity, uint range)
+        {
+            var entityPositionX = entity.Position.X + xOffset;
+            var entityPositionY = entity.Position.Y + yOffset;
+            var oldRange = entity.Range;
+            var id = entity.Id;
+            var type = entity.Type;
+
+            var oldSquareMaxX = entityPositionX + oldRange;
+            var oldSquareMaxY = entityPositionY + oldRange;
+            var oldSquareMinX = entityPositionX - oldRange;
+            var oldSquareMinY = entityPositionY - oldRange;
+
+            var newSquareMaxX = entityPositionX + range;
+            var newSquareMaxY = entityPositionY + range;
+            var newSquareMinX = entityPositionX - range;
+            var newSquareMinY = entityPositionY - range;
+
+            if (range == 0 || oldSquareMinX < 0 || oldSquareMinY < 0 ||
+                oldSquareMaxX > maxX ||
+                oldSquareMaxY > maxY ||
+                newSquareMinX < 0 || newSquareMinY < 0 ||
+                newSquareMaxX > maxX ||
+                newSquareMaxY > maxY) return;
+
+            // we actually have a circle but we use this as a square for performance reasons
+            // we now find all areas that are inside this square
+            // We first use starting y index to start filling
+            var oldStartingYIndex = (int) Math.Floor(oldSquareMinY / areaSize);
+            // We now define starting x index to start filling
+            var oldStartingXIndex = (int) Math.Floor(oldSquareMinX / areaSize);
+            // Also define stopping indexes
+            var oldStoppingYIndex =
+                (int) Math.Ceiling(oldSquareMaxY / areaSize);
+            var oldStoppingXIndex =
+                (int) Math.Ceiling(oldSquareMaxX / areaSize);
+
+            // we actually have a circle but we use this as a square for performance reasons
+            // we now find all areas that are inside this square
+            // We first use starting y index to start filling
+            var newStartingYIndex = (int) Math.Floor(newSquareMinY / areaSize);
+            // We now define starting x index to start filling
+            var newStartingXIndex = (int) Math.Floor(newSquareMinX / areaSize);
+            // Also define stopping indexes
+            var newStoppingYIndex =
+                (int) Math.Ceiling(newSquareMaxY / areaSize);
+            var newStoppingXIndex =
+                (int) Math.Ceiling(newSquareMaxX / areaSize);
+
+            for (var j = oldStartingXIndex; j <= oldStoppingXIndex; j++)
+            {
+                var xArr = entityAreas[j];
+                for (var i = oldStartingYIndex; i <= oldStoppingYIndex; i++)
+                {
+                    //TODO: Now we check if (i,j) is inside the new position range, so we don't have to delete it
+                    var arr = xArr[i];
+                    var length = arr.Count;
+                    int k;
+                    var found = false;
+                    for (k = 0; k < length; k++)
+                    {
+                        var currEntity = arr[k];
+                        if (currEntity.Id != id || currEntity.Type != type) continue;
+                        found = true;
+                        break;
+                    }
+
+                    if (!found) continue;
+                    xArr[i].RemoveAt(k);
+                }
+            }
+
+            for (var j = newStartingXIndex; j <= newStoppingXIndex; j++)
+            {
+                var xArr = entityAreas[j];
+                for (var i = newStartingYIndex; i <= newStoppingYIndex; i++)
+                {
+                    xArr[i].Add(entity);
+                }
+            }
+        }
+
+        public override void UpdateEntityDimension(IEntity entity, int dimension)
+        {
+            // This algorithm doesn't has different memory layout depending on dimension
+        }
+
+        /*
+        X can see only X
+        -X can see 0 and -X
+        0 can't see -X and X
+        */
+        protected static bool CanSeeOtherDimension(int dimension, int otherDimension)
+        {
+            if (dimension > 0) return dimension == otherDimension;
+            if (dimension < 0) return otherDimension == 0 || dimension == otherDimension;
+            if (dimension == 0) return otherDimension == 0;
+            return false;
+        }
+
+        //TODO: check if we can find a better way to pass a position and e.g. improve performance of this method by return type ect.
+        public override IEnumerable<IEntity> Find(Vector3 position, int dimension)
+        {
+            var posX = position.X + xOffset;
+            var posY = position.Y + yOffset;
+
+            var xIndex = (int) Math.Floor(posX / areaSize);
+
+            var yIndex = (int) Math.Floor(posY / areaSize);
+
+            if (xIndex < 0 || yIndex < 0 || xIndex >= maxXAreaIndex || yIndex >= maxYAreaIndex) yield break;
+
+            // x2 and y2 only required for complete exact range check
+
+            /*var x2Index = (int) Math.Ceiling(posX / areaSize);
+
+            var y2Index = (int) Math.Ceiling(posY / areaSize);*/
+
+            var areaEntities = entityAreas[xIndex][yIndex];
+
+            for (int j = 0, innerLength = areaEntities.Count; j < innerLength; j++)
+            {
+                var entity = areaEntities[j];
+                if (Vector3.DistanceSquared(entity.Position, position) > entity.RangeSquared ||
+                    !CanSeeOtherDimension(entity.Dimension, dimension)) continue;
+                yield return entity;
+            }
+
+            /*if (xIndex != x2Index && yIndex == y2Index)
+            {
+                var innerAreaEntities = entityAreas[x2Index][yIndex];
+
+                for (int j = 0, innerLength = innerAreaEntities.Length; j < innerLength; j++)
+                {
+                    var entity = innerAreaEntities[j];
+                    if (Vector3.Distance(entity.Position, position) > entity.Range) continue;
+                    callback(entity);
+                }
+            } else if (xIndex == x2Index && yIndex != y2Index)
+            {
+                var innerAreaEntities = entityAreas[xIndex][y2Index];
+
+                for (int j = 0, innerLength = innerAreaEntities.Length; j < innerLength; j++)
+                {
+                    var entity = innerAreaEntities[j];
+                    if (Vector3.Distance(entity.Position, position) > entity.Range) continue;
+                    callback(entity);
+                }
+            } else if (xIndex != x2Index && yIndex != y2Index)
+            {
+                var innerAreaEntities = entityAreas[x2Index][yIndex];
+
+                for (int j = 0, innerLength = innerAreaEntities.Length; j < innerLength; j++)
+                {
+                    var entity = innerAreaEntities[j];
+                    if (Vector3.Distance(entity.Position, position) > entity.Range) continue;
+                    callback(entity);
+                }
+                
+                innerAreaEntities = entityAreas[x2Index][y2Index];
+
+                for (int j = 0, innerLength = innerAreaEntities.Length; j < innerLength; j++)
+                {
+                    var entity = innerAreaEntities[j];
+                    if (Vector3.Distance(entity.Position, position) > entity.Range) continue;
+                    callback(entity);
+                }
+                
+                innerAreaEntities = entityAreas[xIndex][y2Index];
+
+                for (int j = 0, innerLength = innerAreaEntities.Length; j < innerLength; j++)
+                {
+                    var entity = innerAreaEntities[j];
+                    if (Vector3.Distance(entity.Position, position) > entity.Range) continue;
+                    callback(entity);
+                }
+            }*/
+        }
+    }
+}
