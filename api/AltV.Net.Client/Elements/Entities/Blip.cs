35e519ab751df09ddb2f72fa30188c84a02f773a
Solid_Snake <ocelotetv@gmail.com>
2020-03-10 14:16:21 +0100

    Updatet Blip.cs
    Little but important fix

--- api/AltV.Net.Client/Elements/Entities/Blip.cs
+++ api/AltV.Net.Client/Elements/Entities/Blip.cs
@@ -8,152 +8,152 @@ namespace AltV.Net.Client.Elements.Entities
         public int Alpha
         {
             get { return (int)jsObject.GetObjectProperty("alpha"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("alpha", value); }
+            set { jsObject.SetObjectProperty("x", value); }
         }
         public bool AsMissionCreator
         {
             get { return (bool)jsObject.GetObjectProperty("asMissionCreator"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("asMissionCreator", value); }
+            set { jsObject.SetObjectProperty("asMissionCreator", value); }
         }
         public bool Bright
         {
             get { return (bool)jsObject.GetObjectProperty("bright"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("bright", value); }
+            set { jsObject.SetObjectProperty("bright", value); }
         }
         public int Category
         {
             get { return (int)jsObject.GetObjectProperty("category"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("category", value); }
+            set { jsObject.SetObjectProperty("category", value); }
         }
         public int Color
         {
             get { return (int)jsObject.GetObjectProperty("color"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("color", value); }
+            set { jsObject.SetObjectProperty("color", value); }
         }
         public bool CrewIndicatorVisible
         {
             get { return (bool)jsObject.GetObjectProperty("crewIndicatorVisible"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("crewIndicatorVisible", value); }
+            set { jsObject.SetObjectProperty("crewIndicatorVisible", value); }
         }
         public int FlashInterval
         {
             get { return (int)jsObject.GetObjectProperty("flashInterval"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("flashInterval", value); }
+            set { jsObject.SetObjectProperty("flashInterval", value); }
         }
         public int FlashTimer
         {
             get { return (int)jsObject.GetObjectProperty("flashTimer"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("flashTimer", value); }
+            set { jsObject.SetObjectProperty("flashTimer", value); }
         }
         public bool Flashes
         {
             get { return (bool)jsObject.GetObjectProperty("flashes"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("flashes", value); }
+            set { jsObject.SetObjectProperty("flashes", value); }
         }
         public bool FlashesAlternate
         {
             get { return (bool)jsObject.GetObjectProperty("flashesAlternate"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("flashesAlternate", value); }
+            set { jsObject.SetObjectProperty("flashesAlternate", value); }
         }
         public bool FriendIndicatorVisible
         {
             get { return (bool)jsObject.GetObjectProperty("friendIndicatorVisible"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("friendIndicatorVisible", value); }
+            set { jsObject.SetObjectProperty("friendIndicatorVisible", value); }
         }
         public bool Friendly
         {
             get { return (bool)jsObject.GetObjectProperty("friendly"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("friendly", value); }
+            set { jsObject.SetObjectProperty("friendly", value); }
         }
         public string GxtName
         {
             get { return (string)jsObject.GetObjectProperty("gxtName"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("gxtName", value); }
+            set { jsObject.SetObjectProperty("gxtName", value); }
         }
         public float Heading
         {
             get { return (float)jsObject.GetObjectProperty("heading"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("heading", value); }
+            set { jsObject.SetObjectProperty("heading", value); }
         }
         public bool HeadingIndicatorVisible
         {
             get { return (bool)jsObject.GetObjectProperty("headingIndicatorVisible"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("headingIndicatorVisible", value); }
+            set { jsObject.SetObjectProperty("headingIndicatorVisible", value); }
         }
         public bool HighDetail
         {
             get { return (bool)jsObject.GetObjectProperty("highDetail"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("highDetail", value); }
+            set { jsObject.SetObjectProperty("highDetail", value); }
         }
         public string Name
         {
             get { return (string)jsObject.GetObjectProperty("name"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("name", value); }
+            set { jsObject.SetObjectProperty("name", value); }
         }
         public int Number
         {
             get { return (int)jsObject.GetObjectProperty("number"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("number", value); }
+            set { jsObject.SetObjectProperty("number", value); }
         }
         public bool OutlineIndicatorVisible
         {
             get { return (bool)jsObject.GetObjectProperty("outlineIndicatorVisible"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("outlineIndicatorVisible", value); }
+            set { jsObject.SetObjectProperty("outlineIndicatorVisible", value); }
         }
         public int Priority
         {
             get { return (int)jsObject.GetObjectProperty("priority"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("priority", value); }
+            set { jsObject.SetObjectProperty("priority", value); }
         }
         public bool Pulse
         {
             get { return (bool)jsObject.GetObjectProperty("pulse"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("pulse", value); }
+            set { jsObject.SetObjectProperty("pulse", value); }
         }        
         public bool Route
         {
             get { return (bool)jsObject.GetObjectProperty("route"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("route", value); }
+            set { jsObject.SetObjectProperty("route", value); }
         }
         public int RouteColor
         {
             get { return (int)jsObject.GetObjectProperty("routeColor"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("routeColor", value); }
+            set { jsObject.SetObjectProperty("routeColor", value); }
         }
         public float Scale
         {
             get { return (float)jsObject.GetObjectProperty("scale"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("scale", value); }
+            set { jsObject.SetObjectProperty("scale", value); }
         }
         public int SecondaryColor
         {
             get { return (int)jsObject.GetObjectProperty("secondaryColor"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("secondaryColor", value); }
+            set { jsObject.SetObjectProperty("secondaryColor", value); }
         }
         public bool ShortRange
         {
             get { return (bool)jsObject.GetObjectProperty("shortRange"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("shortRange", value); }
+            set { jsObject.SetObjectProperty("shortRange", value); }
         }
         public bool ShowCone
         {
             get { return (bool)jsObject.GetObjectProperty("showCone"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("showCone", value); }
+            set { jsObject.SetObjectProperty("showCone", value); }
         }
         public bool Shrinked
         {
             get { return (bool)jsObject.GetObjectProperty("shrinked"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("shrinked", value); }
+            set { jsObject.SetObjectProperty("shrinked", value); }
         }        
         public int Sprite
         {
             get { return (int)jsObject.GetObjectProperty("sprite"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("sprite", value); }
+            set { jsObject.SetObjectProperty("sprite", value); }
         }
         public bool TickVisible
         {
             get { return (bool)jsObject.GetObjectProperty("tickVisible"); }
-            set { JSObject JsObj = Runtime.NewJSObject(); JsObj.SetObjectProperty("tickVisible", value); }
+            set { jsObject.SetObjectProperty("tickVisible", value); }
         }
         public Blip(JSObject jsObject): base(jsObject){}
     }
