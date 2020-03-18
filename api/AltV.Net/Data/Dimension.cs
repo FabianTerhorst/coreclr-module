e039112c583f3a1987815fc20e6a69a6e710f74c
fabian <fabian.terhorst@gmail.com>
2020-03-18 19:54:01 +0100

    update cpp sdk

---
+++ api/AltV.Net/Data/Dimension.cs
@@ -0,0 +1,8 @@
+namespace AltV.Net.Data
+{
+    public static class Dimension
+    {
+        public static int DefaultDimension = 0;
+        public static int GlobalDimension = -2147483648;
+    }
+}
