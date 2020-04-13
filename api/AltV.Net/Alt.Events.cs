e87b450625abd7bc68ed6b8900d36b7e5c2dd13f
TheMysteriousVincent <agpheaddikilla@yahoo.de>
2020-04-13 22:12:09 +0200

    add entity data missing comments

--- api/AltV.Net/Alt.Events.cs
+++ api/AltV.Net/Alt.Events.cs
@@ -134,12 +134,20 @@ namespace AltV.Net
             remove => Module.ConsoleCommandEventHandler.Remove(value);
         }
 
+        /// <summary>
+        /// Event that is invoked when the meta data of an entity has changed.
+        /// </summary>
+        /// <remarks>Meta data is accessible across different serverside resources.</remarks>
         public static event MetaDataChangeDelegate OnMetaDataChange
         {
             add => Module.MetaDataChangeEventHandler.Add(value);
             remove => Module.MetaDataChangeEventHandler.Remove(value);
         }
 
+        /// <summary>
+        /// Event that is invoked when the synced meta data of an entity has changed.
+        /// </summary>
+        /// <remarks>Synced meta data is accessible across different serverside resources and to all clients without range limitation.</remarks>
         public static event MetaDataChangeDelegate OnSyncedMetaDataChange
         {
             add => Module.SyncedMetaDataChangeEventHandler.Add(value);
