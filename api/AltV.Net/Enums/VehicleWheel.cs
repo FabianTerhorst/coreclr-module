ca8efebdac8073cb991180dda1e50ddb1d54c572
BuddiesTV <BuddiesTV@outlook.de>
2021-10-27 14:41:43 +0200

    Update VehicleWheel.cs

--- api/AltV.Net/Enums/VehicleWheel.cs
+++ api/AltV.Net/Enums/VehicleWheel.cs
@@ -11,6 +11,12 @@ namespace AltV.Net.Enums
         LeftFront,
         RightFront,
         LeftRear,
-        RightRear
+        RightRear,
+        Unk1,
+        Unk2,
+        Unk3,
+        Unk4,
+        Unk5,
+        Unk6
     }
 }
