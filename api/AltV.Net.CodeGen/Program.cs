d823687fa6a6af0a88b7c99d1b0e64ed006c14c2
Dzhemesiuk Artem <programming@zziger.me>
2022-03-05 18:16:59 +0200

    codegen: Compilation fix

--- api/AltV.Net.CodeGen/Program.cs
+++ api/AltV.Net.CodeGen/Program.cs
@@ -92,4 +92,11 @@ public static class StringExtensions
             "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
             _ => input.First().ToString().ToUpper() + input.Substring(1).ToLower()
         };
+    public static string FirstCharToUpper(this string input) =>
+        input switch
+        {
+            null => throw new ArgumentNullException(nameof(input)),
+            "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+            _ => input.First().ToString().ToUpper() + input.Substring(1)
+        };
 }
