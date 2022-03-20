7bfcc26d03e25c56cfe68b3e3b8d05cad9636225
Dzhemesiuk Artem <programming@zziger.me>
2022-03-20 22:40:00 +0200

    Shared IEventHandler

--- api/AltV.Net.Shared/AltShared.cs
+++ api/AltV.Net.Shared/AltShared.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("AltV.Net")]
+[assembly: InternalsVisibleTo("AltV.Net.Async")]
 [assembly: InternalsVisibleTo("AltV.Net.Client")]
 
 namespace AltV.Net.Shared
