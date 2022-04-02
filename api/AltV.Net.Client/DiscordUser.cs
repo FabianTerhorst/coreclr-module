8568252cf8aeee2a50bf429097c9fb8183307378
Dzhemesiuk Artem <programming@zziger.me>
2022-04-02 20:46:31 +0300

    client: api: Added DiscordUser.AvatarUrl getter

--- api/AltV.Net.Client/DiscordUser.cs
+++ api/AltV.Net.Client/DiscordUser.cs
@@ -16,5 +16,15 @@ namespace AltV.Net.Client
         
         [MarshalAs(UnmanagedType.LPStr)]
         public readonly string Avatar;
+        
+        public string AvatarUrl
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(Avatar)) return Avatar.StartsWith("a") ? $"https://cdn.discordapp.com/avatars/{Id}/{Avatar}.gif?size=4096" : $"https://cdn.discordapp.com/avatars/{Id}/{Avatar}.png?size=4096";
+                if (!int.TryParse(Discriminator, out var discriminator)) discriminator = 0;
+                return $"https://cdn.discordapp.com/embed/avatars/{discriminator % 5}.png";
+            }
+        }
     }
 }
