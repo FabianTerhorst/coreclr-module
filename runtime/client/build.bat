04bff9bba7c000a9e3b6de37f92313b259459f4d
Dzhemesiuk Artem <programming@zziger.me>
2022-04-02 15:39:57 +0300

    client: build: Added debug symbols to the build

--- runtime/client/build.bat
+++ runtime/client/build.bat
@@ -10,7 +10,8 @@ IF NOT EXIST build\coreclr-client-module (
 )
 
 cmake . -Bcmake-build-release
-cmake --build cmake-build-release --config Release
-copy .\cmake-build-release\Release\core_clr_client.dll .\build\coreclr-client-module\coreclr-client-module.dll
+cmake --build cmake-build-release --config RelWithDebInfo
+copy .\cmake-build-release\RelWithDebInfo\core_clr_client.dll .\build\coreclr-client-module\coreclr-client-module.dll
+copy .\cmake-build-release\RelWithDebInfo\core_clr_client.pdb .\build\coreclr-client-module\coreclr-client-module.pdb
 copy .\thirdparty\coreclr\nethost.dll .\build\coreclr-client-module
 copy .\thirdparty\coreclr\AltV.Net.Client.runtimeconfig.json .\build\coreclr-client-module
