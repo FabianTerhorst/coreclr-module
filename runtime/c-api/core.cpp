d0bac8bab380d22457f4769ac48470db0c3b7478
JuzTim <wuestenhagen.tim@gmail.com>
2022-04-02 15:52:49 +0200

    [Client] Remove debug log

--- runtime/c-api/core.cpp
+++ runtime/c-api/core.cpp
@@ -509,7 +509,6 @@ void Core_DeallocDiscordUser(ClrDiscordUser* user) {
 }
 
 void Core_ShowCursor(alt::ICore* core, alt::IResource* resource, bool state) {
-    Log::Info << "Calling ShowCursor (" << state << ")" << Log::Endl;
     if(!resource->ToggleCursor(state))
     {
         Log::Warning << "Cursor state can't go < 0" << Log::Endl;
