99a441c7a63b4741d5c73e08515e98a16867e50c
JuzTim <wuestenhagen.tim@gmail.com>
2022-04-10 16:18:59 +0200

    [Client] Fix duplicate cases

--- runtime/client/src/runtime/CSharpResourceImpl.cpp
+++ runtime/client/src/runtime/CSharpResourceImpl.cpp
@@ -35,20 +35,15 @@ bool CSharpResourceImpl::OnEvent(const alt::CEvent* ev)
     if (ev == nullptr) return true;
     auto scope = resource->PushNativesScope();
     switch(ev->GetType()) {
-        case alt::CEvent::Type::SERVER_SCRIPT_EVENT:
-        {
-            auto serverScriptEvent = (alt::CServerScriptEvent*)ev;
+        case alt::CEvent::Type::SERVER_SCRIPT_EVENT: {
+            auto serverScriptEvent = (alt::CServerScriptEvent *) ev;
             alt::MValueArgs serverArgs = serverScriptEvent->GetArgs();
             uint64_t size = serverArgs.GetSize();
-            if(size == 0)
-            {
+            if (size == 0) {
                 OnServerEventDelegate(serverScriptEvent->GetName().CStr(), nullptr, 0);
-            }
-            else
-            {
-                auto constArgs = new alt::MValueConst*[size];
-                for(uint64_t i = 0; i < size; i++)
-                {
+            } else {
+                auto constArgs = new alt::MValueConst *[size];
+                for (uint64_t i = 0; i < size; i++) {
                     constArgs[i] = &serverArgs[i];
                 }
                 OnServerEventDelegate(serverScriptEvent->GetName().CStr(), constArgs, size);
@@ -56,20 +51,15 @@ bool CSharpResourceImpl::OnEvent(const alt::CEvent* ev)
             }
             break;
         }
-        case alt::CEvent::Type::CLIENT_SCRIPT_EVENT:
-        {
-            auto clientScriptEvent = (alt::CClientScriptEvent*)ev;
+        case alt::CEvent::Type::CLIENT_SCRIPT_EVENT: {
+            auto clientScriptEvent = (alt::CClientScriptEvent *) ev;
             alt::MValueArgs serverArgs = clientScriptEvent->GetArgs();
             uint64_t size = serverArgs.GetSize();
-            if(size == 0)
-            {
+            if (size == 0) {
                 OnClientEventDelegate(clientScriptEvent->GetName().CStr(), nullptr, 0);
-            }
-            else
-            {
-                auto constArgs = new alt::MValueConst*[size];
-                for(uint64_t i = 0; i < size; i++)
-                {
+            } else {
+                auto constArgs = new alt::MValueConst *[size];
+                for (uint64_t i = 0; i < size; i++) {
                     constArgs[i] = &serverArgs[i];
                 }
                 OnClientEventDelegate(clientScriptEvent->GetName().CStr(), constArgs, size);
@@ -77,14 +67,12 @@ bool CSharpResourceImpl::OnEvent(const alt::CEvent* ev)
             }
             break;
         }
-        case alt::CEvent::Type::CONSOLE_COMMAND_EVENT:
-        {
-            auto consoleCommandEvent = (alt::CConsoleCommandEvent*)ev;
+        case alt::CEvent::Type::CONSOLE_COMMAND_EVENT: {
+            auto consoleCommandEvent = (alt::CConsoleCommandEvent *) ev;
             auto args = consoleCommandEvent->GetArgs();
             uint64_t size = args.size();
-            auto cArgs = new const char*[size];
-            for(uint64_t i = 0; i < size; i++)
-            {
+            auto cArgs = new const char *[size];
+            for (uint64_t i = 0; i < size; i++) {
                 cArgs[i] = args[i].c_str();
             }
             auto name = consoleCommandEvent->GetName();
@@ -92,16 +80,14 @@ bool CSharpResourceImpl::OnEvent(const alt::CEvent* ev)
             delete[] cArgs;
             break;
         }
-        case alt::CEvent::Type::WEB_VIEW_EVENT:
-        {
-            auto webViewEvent = (alt::CWebViewEvent*)ev;
+        case alt::CEvent::Type::WEB_VIEW_EVENT: {
+            auto webViewEvent = (alt::CWebViewEvent *) ev;
             auto args = webViewEvent->GetArgs();
             auto name = webViewEvent->GetName();
             auto size = args.GetSize();
-            auto constArgs = new alt::MValueConst*[size];
+            auto constArgs = new alt::MValueConst *[size];
 
-            for (auto i = 0; i < size; i++)
-            {
+            for (auto i = 0; i < size; i++) {
                 constArgs[i] = &args[i];
             }
             OnWebViewEventDelegate(webViewEvent->GetTarget().Get(), name.CStr(), constArgs, size);
@@ -109,37 +95,34 @@ bool CSharpResourceImpl::OnEvent(const alt::CEvent* ev)
             break;
         }
 #pragma region Player Events
-		case alt::CEvent::Type::SPAWNED:
-	    {
+        case alt::CEvent::Type::SPAWNED: {
             OnPlayerSpawnDelegate();
             break;
-	    }
-        case alt::CEvent::Type::DISCONNECT_EVENT:
-        {
+        }
+        case alt::CEvent::Type::DISCONNECT_EVENT: {
             OnPlayerDisconnectDelegate();
             break;
         }
-        case alt::CEvent::Type::PLAYER_ENTER_VEHICLE:
-        {
-            auto playerEnterVehicleEvent = (alt::CPlayerEnterVehicleEvent*)ev;
-            OnPlayerEnterVehicleDelegate(playerEnterVehicleEvent->GetTarget().Get(), playerEnterVehicleEvent->GetSeat());
+        case alt::CEvent::Type::PLAYER_ENTER_VEHICLE: {
+            auto playerEnterVehicleEvent = (alt::CPlayerEnterVehicleEvent *) ev;
+            OnPlayerEnterVehicleDelegate(playerEnterVehicleEvent->GetTarget().Get(),
+                                         playerEnterVehicleEvent->GetSeat());
             break;
         }
 #pragma endregion
 #pragma region Entity events
-        case alt::CEvent::Type::GAME_ENTITY_CREATE:
-        {
-            auto gameEntityCreateEvent = (alt::CGameEntityCreateEvent*)ev;
+        case alt::CEvent::Type::GAME_ENTITY_CREATE: {
+            auto gameEntityCreateEvent = (alt::CGameEntityCreateEvent *) ev;
             auto entity = gameEntityCreateEvent->GetTarget().Get();
             auto type = (uint8_t) entity->GetType();
-            void* ptr;
+            void *ptr;
 
             switch (entity->GetType()) {
                 case alt::IBaseObject::Type::PLAYER:
-                    ptr = dynamic_cast<alt::IPlayer*>(entity);
+                    ptr = dynamic_cast<alt::IPlayer *>(entity);
                     break;
                 case alt::IBaseObject::Type::VEHICLE:
-                    ptr = dynamic_cast<alt::IVehicle*>(entity);
+                    ptr = dynamic_cast<alt::IVehicle *>(entity);
                     break;
                 default:
                     ptr = nullptr;
@@ -148,19 +131,18 @@ bool CSharpResourceImpl::OnEvent(const alt::CEvent* ev)
             OnGameEntityCreateDelegate(ptr, type);
             break;
         }
-        case alt::CEvent::Type::GAME_ENTITY_DESTROY:
-        {
-            auto gameEntityDestroyEvent = (alt::CGameEntityDestroyEvent*)ev;
+        case alt::CEvent::Type::GAME_ENTITY_DESTROY: {
+            auto gameEntityDestroyEvent = (alt::CGameEntityDestroyEvent *) ev;
             auto entity = gameEntityDestroyEvent->GetTarget().Get();
             auto type = (uint8_t) entity->GetType();
-            void* ptr;
+            void *ptr;
 
             switch (entity->GetType()) {
                 case alt::IBaseObject::Type::PLAYER:
-                    ptr = dynamic_cast<alt::IPlayer*>(entity);
+                    ptr = dynamic_cast<alt::IPlayer *>(entity);
                     break;
                 case alt::IBaseObject::Type::VEHICLE:
-                    ptr = dynamic_cast<alt::IVehicle*>(entity);
+                    ptr = dynamic_cast<alt::IVehicle *>(entity);
                     break;
                 default:
                     ptr = nullptr;
@@ -171,127 +153,31 @@ bool CSharpResourceImpl::OnEvent(const alt::CEvent* ev)
         }
 #pragma endregion
 #pragma region Misc
-        case alt::CEvent::Type::RESOURCE_ERROR:
-        {
-            auto resourceErrorEvent = (alt::CResourceErrorEvent*)ev;
+        case alt::CEvent::Type::RESOURCE_ERROR: {
+            auto resourceErrorEvent = (alt::CResourceErrorEvent *) ev;
             OnResourceErrorDelegate(resourceErrorEvent->GetResource()->GetName().CStr());
             break;
         }
-        case alt::CEvent::Type::RESOURCE_START:
-        {
-            auto resourceStartEvent = (alt::CResourceStartEvent*)ev;
+        case alt::CEvent::Type::RESOURCE_START: {
+            auto resourceStartEvent = (alt::CResourceStartEvent *) ev;
             OnResourceStartDelegate(resourceStartEvent->GetResource()->GetName().CStr());
             break;
         }
-        case alt::CEvent::Type::RESOURCE_STOP:
-        {
-            auto resourceStopEvent = (alt::CResourceStopEvent*)ev;
+        case alt::CEvent::Type::RESOURCE_STOP: {
+            auto resourceStopEvent = (alt::CResourceStopEvent *) ev;
             OnResourceStopDelegate(resourceStopEvent->GetResource()->GetName().CStr());
             break;
         }
-        case alt::CEvent::Type::KEYBOARD_EVENT:
-        {
-            auto keyboardEvent = (alt::CKeyboardEvent*)ev;
+        case alt::CEvent::Type::KEYBOARD_EVENT: {
+            auto keyboardEvent = (alt::CKeyboardEvent *) ev;
             if (keyboardEvent->GetKeyState() == alt::CKeyboardEvent::KeyState::UP)
                 OnKeyUpDelegate(keyboardEvent->GetKeyCode());
             else
                 OnKeyDownDelegate(keyboardEvent->GetKeyCode());
             break;
         }
-
 #pragma endregion
-        case alt::CEvent::Type::NONE:
-            break;
-        case alt::CEvent::Type::SERVER_STARTED:
-            break;
-        case alt::CEvent::Type::PLAYER_CONNECT:
-            break;
-        case alt::CEvent::Type::PLAYER_BEFORE_CONNECT:
-            break;
-        case alt::CEvent::Type::PLAYER_DISCONNECT:
-            break;
-        case alt::CEvent::Type::CONNECTION_QUEUE_ADD:
-            break;
-        case alt::CEvent::Type::CONNECTION_QUEUE_REMOVE:
-            break;
-        case alt::CEvent::Type::META_CHANGE:
-            break;
-        case alt::CEvent::Type::SYNCED_META_CHANGE:
-            break;
-        case alt::CEvent::Type::STREAM_SYNCED_META_CHANGE:
-            break;
-        case alt::CEvent::Type::GLOBAL_META_CHANGE:
-            break;
-        case alt::CEvent::Type::GLOBAL_SYNCED_META_CHANGE:
-            break;
-        case alt::CEvent::Type::LOCAL_SYNCED_META_CHANGE:
-            break;
-        case alt::CEvent::Type::PLAYER_DAMAGE:
-            break;
-        case alt::CEvent::Type::PLAYER_DEATH:
-            break;
-        case alt::CEvent::Type::FIRE_EVENT:
-            break;
-        case alt::CEvent::Type::EXPLOSION_EVENT:
-            break;
-        case alt::CEvent::Type::START_PROJECTILE_EVENT:
-            break;
-        case alt::CEvent::Type::WEAPON_DAMAGE_EVENT:
-            break;
-        case alt::CEvent::Type::VEHICLE_DESTROY:
-            break;
-        case alt::CEvent::Type::VEHICLE_DAMAGE:
-            break;
-        case alt::CEvent::Type::CHECKPOINT_EVENT:
-            break;
-        case alt::CEvent::Type::COLSHAPE_EVENT:
-            break;
-        case alt::CEvent::Type::PLAYER_ENTERING_VEHICLE:
-            break;
-        case alt::CEvent::Type::PLAYER_LEAVE_VEHICLE:
-            break;
-        case alt::CEvent::Type::PLAYER_CHANGE_VEHICLE_SEAT:
-            break;
-        case alt::CEvent::Type::PLAYER_WEAPON_CHANGE:
-            break;
-        case alt::CEvent::Type::VEHICLE_ATTACH:
-            break;
-        case alt::CEvent::Type::VEHICLE_DETACH:
-            break;
-        case alt::CEvent::Type::NETOWNER_CHANGE:
-            break;
-        case alt::CEvent::Type::REMOVE_ENTITY_EVENT:
-            break;
-        case alt::CEvent::Type::CREATE_BASE_OBJECT_EVENT:
-            break;
-        case alt::CEvent::Type::REMOVE_BASE_OBJECT_EVENT:
-            break;
-        case alt::CEvent::Type::DATA_NODE_RECEIVED_EVENT:
-            break;
-        case alt::CEvent::Type::CONNECTION_COMPLETE:
-            break;
-        case alt::CEvent::Type::GAME_ENTITY_CREATE:
-            break;
-        case alt::CEvent::Type::GAME_ENTITY_DESTROY:
-            break;
-        case alt::CEvent::Type::WEB_SOCKET_CLIENT_EVENT:
-            break;
-        case alt::CEvent::Type::AUDIO_EVENT:
-            break;
-        case alt::CEvent::Type::TASK_CHANGE:
-            break;
-        case alt::CEvent::Type::RMLUI_EVENT:
-            break;
-        case alt::CEvent::Type::WINDOW_FOCUS_CHANGE:
-            break;
-        case alt::CEvent::Type::WINDOW_RESOLUTION_CHANGE:
-            break;
-        case alt::CEvent::Type::ALL:
-            break;
-        case alt::CEvent::Type::SIZE:
-            break;
     }
-
     return true;
 }
 
