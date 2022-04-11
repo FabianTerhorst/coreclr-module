54302873d76939fae4ce8663fc819d448cc1b70f
Dzhemesiuk Artem <programming@zziger.me>
2022-04-11 10:47:33 +0300

    client: api: Fixed TextWriter for non-stirng types

--- api/AltV.Net.Client/Extensions/TextWriterExtensions.cs
+++ api/AltV.Net.Client/Extensions/TextWriterExtensions.cs
@@ -6,7 +6,20 @@ namespace AltV.Net.Client.Extensions;
 public class AltTextWriter : TextWriter
 {
     public override Encoding Encoding { get; } = Encoding.UTF8;
-    
+
+    private string buffer = "";
+    public override void Write(char value)
+    {
+        if (value is '\n')
+        {
+            Alt.LogInfo(buffer);
+            buffer = "";
+            return;
+        }
+        
+        buffer += value;
+    }
+
     public override void WriteLine(string? value)
     {
         if (value == null) return;
@@ -17,7 +30,20 @@ public class AltTextWriter : TextWriter
 public class AltErrorTextWriter : TextWriter
 {
     public override Encoding Encoding { get; } = Encoding.UTF8;
-    
+
+    private string buffer = "";
+    public override void Write(char value)
+    {
+        if (value is '\n' or '\r')
+        {
+            Alt.LogError(buffer);
+            buffer = "";
+            return;
+        }
+        
+        buffer += value;
+    }
+
     public override void WriteLine(string? value)
     {
         if (value == null) return;
