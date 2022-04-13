256d32789f1aa9f48bfc718404d408f5bbb60b6d
JuzTim <wuestenhagen.tim@gmail.com>
2022-04-13 22:17:14 +0200

    [Client + Server] Add LocalPlayer case handling for GetEntityPtr

--- runtime/client/src/runtime/CSharpResourceImpl.cpp
+++ runtime/client/src/runtime/CSharpResourceImpl.cpp
@@ -41,12 +41,11 @@ bool CSharpResourceImpl::Stop()
 void* CSharpResourceImpl::  GetEntityPointer(alt::IEntity* entity) {
     if (entity != nullptr) {
         switch (entity->GetType()) {
+            case alt::IBaseObject::Type::LOCAL_PLAYER:
             case alt::IBaseObject::Type::PLAYER:
                 return dynamic_cast<alt::IPlayer*>(entity);
             case alt::IBaseObject::Type::VEHICLE:
                 return dynamic_cast<alt::IVehicle*>(entity);
-            case alt::IBaseObject::Type::LOCAL_PLAYER:
-                return dynamic_cast<alt::ILocalPlayer*>(entity);
             default:
                 return nullptr;
         }
