0d72bef4d8835ded5f1737c071da0e562c084172
doxoh <till@tillschreiber.de>
2023-02-26 20:01:54 +0100

    mark "Alt.Remove*" Methods obsolete

--- api/AltV.Net/Alt.Entity.cs
+++ api/AltV.Net/Alt.Entity.cs
@@ -1,15 +1,20 @@
+using System;
 using AltV.Net.Elements.Entities;
 
 namespace AltV.Net
 {
     public static partial class Alt
     {
+        [Obsolete("Use vehicle.Destroy() instead")]
         public static void RemoveVehicle(IVehicle vehicle) => vehicle.Destroy();
 
+        [Obsolete("Use blip.Destroy() instead")]
         public static void RemoveBlip(IBlip blip) => blip.Destroy();
 
+        [Obsolete("Use checkpoint.Destroy() instead")]
         public static void RemoveCheckpoint(ICheckpoint checkpoint) => checkpoint.Destroy();
 
+        [Obsolete("Use colShape.Destroy() instead")]
         public static void RemoveColShape(IColShape colShape) => colShape.Destroy();
     }
 }
