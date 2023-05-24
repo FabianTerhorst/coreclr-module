01b4684b5c43bfacd7a7a4a87d935220dfece8de
doxoh <till@tillschreiber.de>
2023-05-24 21:16:49 +0200

    add namespaces there ignored for generate

--- api/AltV.Net.Sdk.Generator/ImplicitUsingsGenerator.cs
+++ api/AltV.Net.Sdk.Generator/ImplicitUsingsGenerator.cs
@@ -5,11 +5,12 @@ namespace AltV.Net.Sdk.Generator
     public class ImplicitUsingsGenerator : IGenerator
     {
         private static readonly Regex NamespaceRegex = new Regex(@"namespace (?<ns>[^\s;\{]+)");
-        
+        private static readonly List<string> IgnoredNamespaces = new() { "AltV.Net.Async.RPC" };
+
         public string Generate(string[] args)
         {
             var usings = new HashSet<string>();
-            
+
             foreach (var project in args)
             {
                 // ([ \t]*)\<!-- ?#GENERATE# (.*?) ?-->
@@ -19,6 +20,9 @@ namespace AltV.Net.Sdk.Generator
                     var ns = NamespaceRegex.Matches(content);
                     foreach (Match match in ns)
                     {
+                        var @namespace = match.Groups["ns"].ToString();
+                        if (IgnoredNamespaces.Contains(@namespace)) continue;
+
                         usings.Add(match.Groups["ns"].ToString());
                     }
                 }
