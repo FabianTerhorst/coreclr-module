2abafb36875f84986c52e72e78019548b5819aa0
doxoh <till@tillschreiber.de>
2023-09-10 21:38:24 +0200

    make WeaponDamage Eventhandling better

---
+++ api/AltV.Net/Data/WeaponDamageResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace AltV.Net.Data;
+
+public record WeaponDamageResponse(bool Cancel, uint? Damage) {
+    public static implicit operator WeaponDamageResponse(bool val) => new WeaponDamageResponse(val, null);
+    public static implicit operator WeaponDamageResponse(uint val) => new WeaponDamageResponse(false, val);
+};
