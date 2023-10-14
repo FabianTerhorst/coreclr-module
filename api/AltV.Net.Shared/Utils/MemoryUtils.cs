40db44665a9bba233aa723f054b0b209f0683c18
doxoh <till@tillschreiber.de>
2023-10-14 11:12:38 +0200

    Revert "by parsing null string, translate it to empty one"
    This reverts commit 45b6ee0fdbf4729732553decbd6bea6fbb0bc71d.

--- api/AltV.Net.Shared/Utils/MemoryUtils.cs
+++ api/AltV.Net.Shared/Utils/MemoryUtils.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Text;
 
 namespace AltV.Net.Shared.Utils
@@ -9,7 +8,10 @@ namespace AltV.Net.Shared.Utils
         //https://github.com/Microsoft/xbox-live-unity-plugin/blob/master/CSharpSource/Source/api/System/MarshallingHelpers.cs
         public static IntPtr StringToHGlobalUtf8(string str)
         {
-            str ??= string.Empty;
+            if (str == null)
+            {
+                return IntPtr.Zero;
+            }
 
             var bytes = Encoding.UTF8.GetBytes(str);
             var pointer = Marshal.AllocHGlobal(bytes.Length + 1);
