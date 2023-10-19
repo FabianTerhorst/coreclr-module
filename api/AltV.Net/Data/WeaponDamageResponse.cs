df87e185b88892640e4e85849967ccd66c48a3db
doxoh <till@tillschreiber.de>
2023-10-19 11:47:27 +0200

    fix weaponDamage by damage value

--- api/AltV.Net/Data/WeaponDamageResponse.cs
+++ api/AltV.Net/Data/WeaponDamageResponse.cs
@@ -2,5 +2,5 @@
 
 public record WeaponDamageResponse(bool notCancel, uint? Damage) {
     public static implicit operator WeaponDamageResponse(bool val) => new WeaponDamageResponse(val, null);
-    public static implicit operator WeaponDamageResponse(uint val) => new WeaponDamageResponse(false, val);
+    public static implicit operator WeaponDamageResponse(uint val) => new WeaponDamageResponse(true, val);
 };
