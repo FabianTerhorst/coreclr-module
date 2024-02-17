93cfd5ab01efcdf9845c07204df54492f03a34e7
Marcel N. <67131061+xLuxy@users.noreply.github.com>
2024-02-17 06:03:16 +0000

    Add Alt.LogColored overload

--- api/AltV.Net/Alt.Log.Colored.cs
+++ api/AltV.Net/Alt.Log.Colored.cs
@@ -4,6 +4,7 @@ namespace AltV.Net
 {
     public static partial class Alt
     {
+        public static void LogColored(string message) => Core.LogColored(message);
         public static void LogColored(ColoredMessage message) => Core.LogColored(message.ToString());
     }
 }
